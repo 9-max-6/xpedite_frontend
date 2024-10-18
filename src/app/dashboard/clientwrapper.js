608fe09d431f669639f2bdf3cbf1f9f327e679ec
9-max-6 <mutukumaxwel@gmail.com>
2024-10-18 10:08:54 +0300

    Fix: updated the supercycles sheet component to fetch data using the client side api. This makes it possible to use the clientwrapper for state management client side.

--- src/app/dashboard/clientwrapper.js
+++ src/app/dashboard/clientwrapper.js
@@ -1,3 +1,4 @@
+'use client';
 import DetailedRequest from './components/oneRequest';
 import CountDown from './components/countdown';
 import GreetingCard from './components/greeting';
@@ -7,7 +8,7 @@ export default function ClientWrapper({ clever }) {
     <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-4 xl:grid-cols-4">
       <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
         <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
-          {/* <GreetingCard clever={clever} /> */}
+          <GreetingCard clever={clever} />
           <CountDown />
         </div>
         <MyCycle clever={clever} />
